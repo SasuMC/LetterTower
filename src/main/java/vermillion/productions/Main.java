4aa0d4782a1145436ffa357675aa85c13f457a20
SasuMC <101135746+SasuMC@users.noreply.github.com>
2025-02-27 13:39:46 -0300

    Removed unnecessary classes

--- src/main/java/vermillion/productions/Main.java
+++ src/main/java/vermillion/productions/Main.java
@@ -6,9 +6,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 import vermillion.productions.plugin.commands.AnswerCommand;
 import vermillion.productions.plugin.commands.PluginCommand;
 import vermillion.productions.utils.CommandUtils;
-import vermillion.productions.utils.ListenerUtils;
-import vermillion.productions.utils.blocks.GhostBlockListeners;
-import vermillion.productions.utils.item.InteractionManager;
 
 public class Main extends JavaPlugin {
 
@@ -17,8 +14,6 @@ public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
-        ListenerUtils.registerListener(new InteractionManager()); // Registra las interacciones.
-        ListenerUtils.registerListener(new GhostBlockListeners()); // Registra los eventos de GhostBlock.
 
         PaperCommandManager commandManager = new PaperCommandManager(this);
         CommandUtils.registerCommands(commandManager, new AnswerCommand(), new PluginCommand());
